5d6591d92742f24be52fa93e4d919e28c7e45930
Deepak <Deepak@DESKTOP-HJN79L4>
2020-04-10 17:13:13 +0530

    first commit

---
+++ tester/src/main/java/com/tester/APITester/ApiTesterApplication.java
@@ -0,0 +1,14 @@
+package com.tester.APITester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTesterApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ApiTesterApplication.class, args);
+	}
+
+}
